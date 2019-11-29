f286bcf12b0c8b58bb33b5c651363f5aaa3ef5ef
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2019-11-29 20:23:26 +0100

    Show skin and place display

--- web/shell_main.js
+++ web/shell_main.js
@@ -32,29 +32,92 @@ function assignImpl(obj, keys, keyI, val) {
     return newObj;
 }
 
-class Screen {
+class Housing {
     constructor() {
-        this.el = redom.el('canvas', {width: 131, height: 16});
-        this.ctx = this.el.getContext('2d');
+        this.el = this.housing = redom.el('div');
     }
 
     update(data) {
-        if (!data.data) {
+        if (!data || !data.skin) {
             return;
         }
-        let imageData = this.ctx.getImageData(0, 0, 131, 16);
-        imageData.data.set(data.data);
-        this.ctx.putImageData(imageData, 0, 0);
+        redom.setAttr(this.housing, {
+            style: {
+                width: data.skin.skin.width + 'px',
+                height: data.skin.skin.height + 'px',
+                'background-image': 'url("Ehrling42sl.gif")'
+            }
+        });
+    }
+}
+
+class Screen {
+    constructor() {
+        this.orig = redom.el('canvas', {width: 131, height: 16});
+        this.el = redom.el('canvas');
+        this.origCtx = this.orig.getContext('2d');
+        this.scaledCtx = this.el.getContext('2d');
+        this.scaledCtx.imageSmoothingEnabled = false;
+        this.scaledCtx.mozImageSmoothingEnabled = false;
+        this.data = {};
+    }
+
+    update(data) {
+        if (!data || !data.data || !data.skin) {
+            return;
+        }
+
+        const width = data.skin.display.scaleX * 131;
+        const height = data.skin.display.scaleY * 16;
+
+        if (this.data.skin !== data.skin) {
+            redom.setAttr(this.el, {
+                style: {
+                    left: data.skin.display.x + 'px',
+                    top: data.skin.display.y + 'px',
+                    position: 'absolute'
+                },
+                width: width,
+                height: height
+            });
+
+            this.scaledCtx.width = width;
+            this.scaledCtx.height = height;
+            this.scaledCtx.resetTransform();
+            this.scaledCtx.scale(data.skin.display.scaleX, data.skin.display.scaleY);
+        }
+
+        if (this.data.data !== data.data) {
+            let imageData = this.origCtx.getImageData(0, 0, 131, 16);
+            imageData.data.set(data.data);
+            this.origCtx.putImageData(imageData, 0, 0);
+            this.scaledCtx.drawImage(this.orig, 0, 0);
+        }
+
+        this.data = data;
     }
 }
 
 class Free42Shell {
     constructor() {
-        this.screen = new Screen();
-        this.el = this.screen;
-        this.data = {
-            screen: {}
-        };
+        this.el = redom.el('div', {style: {position: 'relative'}},
+                           this.housing = new Housing(),
+                           this.screen = new Screen());
+        this.data = {};
+
+        this.loadSkin();
+    }
+
+    async loadSkin() {
+        try {
+            const skin = await loadSkin();
+            console.log('Skin:', skin);
+            let newData = assign(this.data, 'housing.skin', skin);
+            newData = assign(newData, 'screen.skin', skin);
+            this.update(newData);
+        } catch (e) {
+            console.log('Could not load skin', e);
+        }
     }
 
     setScreenPtr(screenPtr) {
@@ -70,6 +133,7 @@ class Free42Shell {
     update(data) {
         this.data = data;
         this.screen.update(this.data.screen);
+        this.housing.update(this.data.housing);
     }
 
 }
@@ -105,22 +169,25 @@ function parsePoint(rect) {
 
 async function loadSkin() {
     const result = {};
-    try {
-        const response = await fetch(SKIN);
-        const text = await response.text();
-        console.log(text);
 
-        const lines = text
-              .split('\n')
-              .map(l => l.trim().replace(/\s*/, ' ').split(' '));
-        for (const line of lines) {
-            if (line.length == 2 && line[0] == 'Skin:') {
-                result.skin = parseRect(line[1]);
-            }
+    const response = await fetch(SKIN);
+    const text = await response.text();
+    console.log(text);
+
+    const lines = text
+          .split('\n')
+          .map(l => l.trim().replace(/\s+/, ' ').split(' '));
+    for (const line of lines) {
+        if (line.length == 2 && line[0] == 'Skin:') {
+            result.skin = parseRect(line[1]);
+        } else if (line.length == 6 && line[0] == 'Display:') {
+            result.display = parsePoint(line[1]);
+            result.display.scaleX = parseInt(line[2], 10);
+            result.display.scaleY = parseInt(line[3], 10);
         }
-    } catch (e) {
-        console.log('Could not load skin', e);
+
     }
+    return result;
 }
 
 redom.mount(document.body, free42Shell);
