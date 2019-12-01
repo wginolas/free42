c71a0b7f3eea3e4700c607fb5294e3865081bb33
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2019-12-01 09:55:04 +0100

    Fix bug in screen repaint. Repaint the whole screen for now

--- web/shell_main.cc
+++ web/shell_main.cc
@@ -63,22 +63,24 @@ extern "C" {
 void shell_print(const char *text, int length,
                  const char *bits, int bytesperline,
                  int x, int y, int width, int height) {
-  
+  printf("shell_print not implemented\n");
 }
 
-void shell_beeper(int frequency, int duration) {}
+void shell_beeper(int frequency, int duration) {
+  printf("shell_beeper not implemented\n");
+}
 
 void shell_blitter(const char *bits, int bytesperline, int x, int y,
                    int width, int height) {
-  for (int yp = 0; yp < height; yp++) {
+  for (int yp = 0; yp < SCREEN_HEIGHT; yp++) {
     char b = 0;
-    for (int xp = 0; xp < width; xp++) {
+    for (int xp = 0; xp < SCREEN_WIDTH; xp++) {
       if (xp % 8 == 0) {
         b = bits[yp * bytesperline + xp / 8];
       }
       int on = b & 1;
       b = b >> 1;
-      int outI = ((y + yp) * SCREEN_WIDTH + x + xp) * 4;
+      int outI = (yp * SCREEN_WIDTH + xp) * 4;
       screen[outI + 0] = on ? onRed : offRed;  // Red
       screen[outI + 1] = on ? onGreen : offGreen;  // Green
       screen[outI + 2] = on ? onBlue : offBlue;  // Blue
@@ -92,10 +94,11 @@ void shell_blitter(const char *bits, int bytesperline, int x, int y,
 }
 
 void shell_message(const char *message) {
-  
+  printf("shell_message not implemented\n");
 }
 
 void shell_powerdown() {
+  printf("shell_powerdown not implemented\n");
 }
 
 int shell_low_battery() {
@@ -103,13 +106,16 @@ int shell_low_battery() {
 }
 
 int8 shell_random_seed() {
+  printf("shell_random_seed not implemented\n");
   return 0;
 }
 
 void shell_annunciators(int updn, int shf, int prt, int run, int g, int rad) {
+  printf("shell_annunciators not implemented\n");
 }
 
 uint4 shell_milliseconds() {
+  printf("shell_milliseconds not implemented\n");
   return 0;
 }
 
@@ -118,11 +124,11 @@ int shell_decimal_point() {
 }
 
 void shell_get_time_date(uint4 *time, uint4 *date, int *weekday) {
-  
+  printf("shell_get_time_date not implemented\n");
 }
 
 void shell_delay(int duration) {
-  
+  printf("shell_delay not implemented\n");
 }
 
 uint4 shell_get_mem() {
@@ -130,9 +136,10 @@ uint4 shell_get_mem() {
 };
 
 int shell_wants_cpu() {
+  printf("shell_wants_cpu not implemented\n");
   return 0;
 }
 
 void shell_request_timeout3(int delay) {
-  
+  printf("shell_request_timeout3 not implemented\n");
 }
